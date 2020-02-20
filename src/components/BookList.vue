7cc78fc43cc76b0c1b5caecf7c784973e1a13769
RealMarkSussex <mark.sussex@gocompare.com>
2020-02-20 15:39:34 +0000

    module 2 (forgot to save)

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,18 +1,24 @@
 <template>
-    
+    <div>
+        <h1>
+            {{title}}
+        </h1>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "BookList"
+    name: "BookList",
     data() {
         return {
             title: "All Books"
-        },
+        }
     },
 }
 </script>
 
 <style scoped>
-
+    h1 h2{
+        font-weight: normal;
+    }
 </style>
