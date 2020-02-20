bda830d77e7eef4ddada5ac58d130868fb00b256
RealMarkSussex <mark.sussex@gocompare.com>
2020-02-20 16:01:33 +0000

    new changes

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,9 +1,17 @@
 <template>
-    
+    <li>{{book.title}}:{{book.author}}</li>
 </template>
 
 <script>
 export default {
-    
+    name: "BookItem",
+    props: ['Book']
 }
 </script>
+
+<style scoped>
+    li {
+        display:block;
+        margin: 0 10px;
+        }
+</style>
