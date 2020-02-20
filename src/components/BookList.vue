88c1dbc546dcb93ddf0f42711290214010db9344
RealMarkSussex <mark.sussex@gocompare.com>
2020-02-20 15:51:05 +0000

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -29,8 +29,12 @@ export default {
     h1, h2{
         font-weight: normal;
     }
-    style {
+    ul {
         list-style-type: none;
         padding: 0;
+    }
+    li {
+        display:block;
+        margin: 0 10px;
     }
 </style>
